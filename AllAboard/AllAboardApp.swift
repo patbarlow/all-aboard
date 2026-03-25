127504160303b08be55dac3aabe27870ac76c20f
PB <141213057+patbarlow@users.noreply.github.com>
2026-03-26 10:51:07 +1100

    Fix Sparkle channel setup to avoid updater delegate compile issues

--- AllAboard/AllAboardApp.swift
+++ AllAboard/AllAboardApp.swift
@@ -1,10 +1,52 @@
 import SwiftUI
 import Sparkle
 
+enum ReleaseChannel: String {
+    case stable
+    case beta
+}
+
+enum AppSettings {
+    static let releaseChannelKey = "release-channel"
+    static let enableBetaFeaturesKey = "enable-beta-features"
+    static let stableFeedURL = "https://raw.githubusercontent.com/patbarlow/all-aboard/main/appcast.xml"
+    static let betaFeedURL = "https://raw.githubusercontent.com/patbarlow/all-aboard/main/appcast-beta.xml"
+
+    static var releaseChannel: ReleaseChannel {
+        get {
+            guard let raw = UserDefaults.standard.string(forKey: releaseChannelKey),
+                  let channel = ReleaseChannel(rawValue: raw) else {
+                return .stable
+            }
+            return channel
+        }
+        set {
+            UserDefaults.standard.set(newValue.rawValue, forKey: releaseChannelKey)
+        }
+    }
+
+    static var enableBetaFeatures: Bool {
+        get { UserDefaults.standard.bool(forKey: enableBetaFeaturesKey) }
+        set { UserDefaults.standard.set(newValue, forKey: enableBetaFeaturesKey) }
+    }
+}
+
 @main
 struct AllAboardApp: App {
     @NSApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
-    private let updaterController = SPUStandardUpdaterController(startingUpdater: true, updaterDelegate: nil, userDriverDelegate: nil)
+    private let updaterController: SPUStandardUpdaterController
+
+    init() {
+        let feedURL: String
+        switch AppSettings.releaseChannel {
+        case .stable:
+            feedURL = AppSettings.stableFeedURL
+        case .beta:
+            feedURL = AppSettings.betaFeedURL
+        }
+        UserDefaults.standard.set(feedURL, forKey: "SUFeedURL")
+        updaterController = SPUStandardUpdaterController(startingUpdater: true, updaterDelegate: nil, userDriverDelegate: nil)
+    }
 
     var body: some Scene {
         Settings {
